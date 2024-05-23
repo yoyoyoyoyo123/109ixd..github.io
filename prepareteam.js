27f3a663d7c0833e2e2abf1a0791634313e359ba
Kenny <kenny91728@gmail.com>
2024-05-23 20:35:37 +0800

    523-changefooter&click

--- prepareteam.js
+++ prepareteam.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
    
     $('.home-img').slick({
-        autoplay: false, 
+        autoplay: true, 
         autoplaySpeed: 2000,
         arrows: false, 
-        dots: true, 
+        dots: false, 
         infinite: true, 
         speed: 500, 
         slidesToShow: 1, 
         slidesToScroll: 1
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const portfolioBoxes = document.querySelectorAll('.portfolio-box');
+
+    portfolioBoxes.forEach(box => {
+        box.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
     });
 });
